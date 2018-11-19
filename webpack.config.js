13f4fc49b21f29f08761526391b22e97e59acf24
Ivan Kleshnin <ivan@paqmind.com>
2018-11-19 14:49:36 +0200

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,60 @@
-const webpack = require('webpack');
+let parseArgv = require("minimist")
+let Webpack = require("webpack")
+
+let argv = parseArgv(process.argv)
+
+let mode = argv.mode == "production" ? "production" : "development"
+let devtool = argv.mode == "production" ? "" : "eval"
 
 module.exports = {
+  mode: mode,
+
+  devtool: devtool,
+
+  target: "web",
+
   entry: './src/index.js',
+
+  output: {
+    // https://webpack.js.org/configuration/output/#output-path
+    path: __dirname + '/public',
+
+    // https://webpack.js.org/configuration/output/#output-publicpath
+    publicPath: '/',
+
+    // https://webpack.js.org/configuration/output/#output-filename
+    filename: 'bundle.js',
+
+    // https://webpack.js.org/configuration/output/#output-chunkfilename
+    // chunkFilename: "[name].chunk.js",
+  },
+
   module: {
     rules: [
+      // JS: https://github.com/babel/babel-loader
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
+
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    alias: {
+      path: "path-webpack", // fix broken Webpack polyfill
+    },
+
+    // https://webpack.js.org/configuration/resolve/#resolve-extensions
+    extensions: [".js", ".jsx", ".json"],
   },
-  output: {
-    path: __dirname + '/public',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
+
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new Webpack.HotModuleReplacementPlugin()
   ],
+
   devServer: {
     contentBase: './public',
     hot: true
   }
-};
+}
